b6c7a34e485cb66c7c52964894765880eef0b3e3
Neko-Sato <hisatosi35350@gmail.com>
2024-09-03 23:22:16 +0900

    update

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 06:12:44 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/09/03 22:21:33 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/09/03 23:21:57 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,25 @@ int ft_stoi(char *str) {
   char *endptr;
   errno = 0;
   long i = std::strtol(str, &endptr, 10);
-  if (errno || std::numeric_limits<int>::max() < i ||
+  if (errno || *endptr || std::numeric_limits<int>::max() < i ||
       std::numeric_limits<int>::min() > i)
-    throw std::runtime_error("Error: Invalid argument");
+    throw std::runtime_error("strtol");
   return i;
 }
 
 int main(int argc, char **argv) {
   std::size_t size = argc - 1;
   int *data = new int[size];
-  for (std::size_t i = 0; i < size; i++)
-    data[i] = ft_stoi(argv[i + 1]);
-  PmergeMe(data, size);
+  try {
+    for (std::size_t i = 0; i < size; i++) {
+      data[i] = ft_stoi(argv[i + 1]);
+      if (data[i] < 0)
+        throw std::runtime_error("Invalid argument");
+    }
+    PmergeMe(data, size);
+  } catch (const std::exception &e) {
+    std::cerr << "Error" << std::endl;
+  }
   delete[] data;
   return 0;
 }
