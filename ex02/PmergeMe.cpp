d8aa8e31fee894837e8b71c0aa01c56f07662e05
Neko-Sato <hisatosi35350@gmail.com>
2024-10-09 01:01:10 +0900

    update

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: hshimizu <hshimizu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 06:30:23 by hshimizu          #+#    #+#             */
-/*   Updated: 2024/10/08 22:12:19 by hshimizu         ###   ########.fr       */
+/*   Updated: 2024/10/09 01:00:30 by hshimizu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,6 +85,7 @@ template <typename T> void sort(std::vector<T> &data) {
     small.push_back(isLess ? &*pre : &*it);
     large.back().push(&small.back());
   }
+std::cout << std::endl;//
   sort(large);
   std::vector<Node<Type> > tmp;
   tmp.reserve(data.size());
@@ -102,6 +103,12 @@ template <typename T> void sort(std::vector<T> &data) {
            jt != tmp.rend();) {
         if (jt->hasPair()) {
           Node<Type> &node = *jt->pop();
+for (typename std::vector<Node<Type> >::iterator kt = tmp.begin(); kt != tmp.end(); ++kt)
+  std::cout << kt->getTypical() << " ";
+std::cout << std::endl;//
+for (typename std::vector<Node<Type> >::iterator kt = tmp.begin(); kt != --jt.base(); ++kt)
+  std::cout << kt->getTypical() << " ";
+std::cout << std::endl;//
           tmp.insert(std::lower_bound(tmp.begin(), --jt.base(), node), node);
         } else
           ++jt;
@@ -109,6 +116,9 @@ template <typename T> void sort(std::vector<T> &data) {
     }
     if (data.size() % 2) {
       Node<Type> &node = small.back();
+for (typename std::vector<Node<Type> >::iterator kt = tmp.begin(); kt != tmp.end(); ++kt)
+  std::cout << kt->getTypical() << " ";
+std::cout << std::endl;//
       tmp.insert(std::lower_bound(tmp.begin(), tmp.end(), node), node);
     }
   }
@@ -121,6 +131,7 @@ template <typename T> void sort(std::vector<T> &data) {
     res.push_back(*value);
   }
   data.swap(res);
+std::cout << std::endl;//
 }
 
 template <typename T> void sort(std::list<T> &data) {
@@ -198,11 +209,49 @@ template <typename T> void sort(std::list<T> &data) {
 // std::size_t Integer::count = 0;
 
 // int main() {
-//   int data[] = {21,1,13,2,17,3,12,4,20,5,15,6,19,7,14,8,18,9,16,10,11};
-//   std::vector<Integer> v1(data, data + sizeof(data) / sizeof(data[0]));
+//   int data[] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+//   std::list<Integer> v1(data, data + sizeof(data) / sizeof(data[0]));
+//   Integer::count = 0;
 //   sort(v1);
-//   for (std::vector<Integer>::iterator it = v1.begin(); it != v1.end(); ++it)
+//   for (std::list<Integer>::iterator it = v1.begin(); it != v1.end(); ++it)
 //     std::cout << *it << " ";
 //   std::cout << std::endl;
 //   return 0;
 // }
+// /*
+// フォールドジョンソンアルゴリズムは最低の比較回数を実現します。
+// 21の時はどうなるか考えてみましょう。
+// まずペアを作ります
+// a a a a a a a a a a
+// b b b b b b b b b b b
+// この時比較が10回行われます。
+//  a a a a a
+//  b b b b b 
+//  この時比較が5回行われます。
+//   a a
+//   b b b
+//   この時比較が2回行われます。
+//    a
+//    b
+//    この時比較が1回行われます。
+//    挿入を行います
+//    c c
+//   挿入を行います
+//  c c a
+//      b b
+//  比較回数2回の挿入が1回行われます。
+//  余りの挿入が行われます。ceil(log2(4+1))=3回の挿入を行われます。
+// c c a a a a
+//     b b b b 
+// 比較回数2回の挿入が2回行われます。
+// 比較回数3回の挿入が2回行われます。
+// c c a a a a a a a a a
+//     b b b b b b b b b b
+// 比較回数2回の挿入が2回行われます。
+// 比較回数3回の挿入が2回行われます。
+// 比較回数4回の挿入が5回行われます。
+// 余りの挿入が行われます。ceil(log2(20+1))=5回の挿入を行われます。
+
+// よって10+5+2+1+2+3+2+2+3+3+2+2+3+3+4+4+4+4+4+5=68
+// の比較が必要になります
+// */
